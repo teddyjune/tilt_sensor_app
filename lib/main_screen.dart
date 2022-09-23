176d5adf3bdfc201a81022f3337ac914ae3e76b8
yeonjoo.lee <teddyjune@naver.com>
2022-09-23 18:35:26 +0900

    feat: 공의 크기만큼 빼줘서 화면 안에 있게 수정

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -16,10 +16,11 @@ class _MainScreenState extends State<MainScreen> {
     super.initState();
   }
 
-  double posX = 180, posY = 350;
-
   @override
   Widget build(BuildContext context) {
+    double posX = MediaQuery.of(context).size.width / 2 - 50;
+    double posY = MediaQuery.of(context).size.height / 2 - 50;
+
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
 
@@ -28,20 +29,19 @@ class _MainScreenState extends State<MainScreen> {
         elevation: 0,
         title: Text(widget.title),
       ),
-      body: StreamBuilder<GyroscopeEvent>(
-          stream: SensorsPlatform.instance.gyroscopeEvents,
+      body: StreamBuilder<AccelerometerEvent>(
+          stream: SensorsPlatform.instance.accelerometerEvents,
           builder: (context, snapshot) {
-            // print("");
             if (snapshot.hasData) {
               posX = posX + (snapshot.data!.x * 10);
               if (posX > width) {
-                posX = width;
+                posX = width-40;
               } else if (posX < 0) {
                 posX = 0;
               }
               posY = posY + (snapshot.data!.y * 10);
               if (posY > height) {
-                posY = height;
+                posY = height-40;
               } else if (posY < 0) {
                 posY = 0;
               }
@@ -57,3 +57,18 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 }
+
+// class DrawCircle extends CustomPaint {
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     var paint = Paint();
+//     paint.color = Colors.red;
+//     canvas.drawCircle(Offset(posX, posY), 50, paint);
+//   }
+//
+//   @override
+//   bool shouldRepaint(covariant CustomPainter oldDelegate) {
+//     //false : paint call might be optimized away.
+//     return false;
+//   }
+// }
